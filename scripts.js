31f58471db8995921a154d7b27c7c29f5632dae2
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-14 12:52:30 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const chances = [
   { chance: 1000, color: 'yellow' },
   { chance: 100, color: 'orange' },
   { chance: 10, color: 'red' },
-  { chance: 5, color: 'gray' },
+  { chance: 5, color: 'gray' }
 ];
 
 function determineChance() {
