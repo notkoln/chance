385e1f2f9c698dd9de798d8ebec8ef117ea0b821
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-14 14:22:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,8 +3,6 @@ if (!spins) {
   localStorage.setItem("spins", 0)
 }
 
-document.title = `Spins: ${spins}`
-
 const chances = [
   { chance: 10000000, color: 'pink' },
   { chance: 1000000, color: 'purple' },
@@ -14,28 +12,33 @@ const chances = [
   { chance: 100, color: 'orange' },
   { chance: 10, color: 'red' },
   { chance: 5, color: 'gray' },
-];
+]
+
+function uas(val) {
+  localStorage.setItem("spins", val)
+  document.title = `Spins: ${spins}`
+}
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
 function determineChance() {
   for (let {chance, color} of chances) {
-    const roll = Math.floor(Math.random() * chance) + 1;
+    const roll = Math.floor(Math.random() * chance) + 1
     if (roll === 1) {
-       const capColor = color[0].toUpperCase() + color.slice(1);
-       document.body.innerText = `${capColor} - 1 in ${numberWithCommas(chance)}`;
-       document.body.style.backgroundColor = color;
-       spins = spins + 1
+       const capColor = color[0].toUpperCase() + color.slice(1)
+       document.body.innerText = `${capColor} - 1 in ${numberWithCommas(chance)}`
+       document.body.style.backgroundColor = color
+       uas(spins + 1)
        
-       return;
+       return
     }
   }
   
   // If here, no hits
-  document.body.innerText = 'Nothing.';
+  document.body.innerText = 'Nothing.'
   spins = spins + 1
 }
 
-determineChance();
+determineChance()
