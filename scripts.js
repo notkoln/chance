ee4ba34de38dcb9f20b369eb677e68af4417cc2e
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-14 13:06:57 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
 const chances = [
+  { chance: 10000000, color: '' },
+  { chance: 1000000, color: 'purple' },
+  { chance: 100000, color: 'blue' },
   { chance: 10000, color: 'green' },
   { chance: 1000, color: 'yellow' },
   { chance: 100, color: 'orange' },
   { chance: 10, color: 'red' },
-  { chance: 5, color: 'gray' }
+  { chance: 5, color: 'gray' },
 ];
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 function determineChance() {
   for (let {chance, color} of chances) {
     const roll = Math.floor(Math.random() * chance) + 1;
     if (roll === 1) {
        const capColor = color[0].toUpperCase() + color.slice(1);
-       document.body.innerText = `${capColor} - 1 in ${chance}`;
+       document.body.innerText = `${capColor} - 1 in ${numberWithCommas(chance)}`;
        document.body.style.backgroundColor = color;
        
        return;
