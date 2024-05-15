44306de6318ea0fd8d50bc472d911a83923cb747
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-15 08:33:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,6 +25,9 @@ function determineChance() {
        const capColor = color[0].toUpperCase() + color.slice(1)
        document.body.innerText = `${capColor} - 1 in ${numberWithCommas(chance)}`
        document.body.style.backgroundColor = color
+       var spints = parseInt(localStorage.getItem("rolls"))
+       localStorage.setItem("rolls", ++spints)
+       document.title = `Spins: ${spints}`
        
        return
     }
@@ -32,9 +35,9 @@ function determineChance() {
   
   // If here, no hits
   document.body.innerText = 'Nothing.'
+  var spints = parseInt(localStorage.getItem("rolls"))
+  localStorage.setItem("rolls", ++spints)
+  document.title = `Spins: ${spints}`
 }
 
 determineChance()
-var spints = parseInt(localStorage.getItem("rolls"))
-localStorage.setItem("rolls", ++spints)
-document.title = `Spins: ${spints}`
