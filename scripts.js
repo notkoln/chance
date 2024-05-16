c205e035dac4578a3d7c6d4ef983140815f5c9a4
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-16 11:08:15 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,8 +17,8 @@ const chances = [
   { chance: 1000, color: 'yellow' },
   { chance: 100, color: 'orange' },
   { chance: 10, color: 'red' },
-  { chance: 5, color: 'tan' },
-  { chance: 3, color: 'gray' },
+  { chance: 5, color: 'olive' },
+  { chance: 3, color: 'tan' },
 ]
 
 function numberWithCommas(x) {
